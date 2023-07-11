9bb5762a95661a796b03c9807a40816eb88d8662
tcher2007 <tcher2007>
2023-07-12 00:44:08 +0800

    修改用戶登入後的介面文字，與MobileService內的水質狀況提供模塊

--- Service/MobileDataProcess.cs
+++ Service/MobileDataProcess.cs
@@ -19,8 +19,36 @@ namespace TestProject.Service
             List<Aquarium> aquariums = db.Aquarium.Where(x => x.Auth001Id == Auth001Id && x.BindTag == "0").ToList();
 
             // 用魚缸編號將該傳回的資料取出
-            foreach (var item in aquariums)
+            //foreach (var item in aquariums)
+            //{
+            //    ViewAquaruimSituation_ForMobile viewAquaruimSituation_ForMobile = db.ViewAquaruimSituation_ForMobile.Where(x => x.Auth001Id == Auth001Id && x.AquariumId == item.Id)
+            //                                                                                                        .OrderByDescending(x => x.createTime)
+            //                                                                                                        .FirstOrDefault();
+            //    if (viewAquaruimSituation_ForMobile == null)
+            //    {
+            //        viewAquaruimSituation_ForMobile = new ViewAquaruimSituation_ForMobile
+            //        {
+            //            Auth001Id = Auth001Id,
+            //            AquariumUnitNum = item.AquariumUnitNum,
+            //            WaterType = "",
+            //            BindTag = "",
+            //            AquariumId = item.Id,
+            //            temperature = "尚未擁有資料",
+            //            Turbidity = "尚未擁有資料",
+            //            PH = "尚未擁有資料",
+            //            TDS = "尚未擁有資料",
+            //            WaterLevel = "",
+            //            createTime = DateTime.Now,
+            //            WaterLevelNum = ""
+            //        };
+            //    }
+
+            //    DataList.Add(viewAquaruimSituation_ForMobile);
+            //}
+
+            for (int i = 0; i < aquariums.Count; i++)
             {
+                var item = aquariums[i];
                 ViewAquaruimSituation_ForMobile viewAquaruimSituation_ForMobile = db.ViewAquaruimSituation_ForMobile.Where(x => x.Auth001Id == Auth001Id && x.AquariumId == item.Id)
                                                                                                                     .OrderByDescending(x => x.createTime)
                                                                                                                     .FirstOrDefault();
@@ -30,7 +58,7 @@ namespace TestProject.Service
                     {
                         Auth001Id = Auth001Id,
                         AquariumUnitNum = item.AquariumUnitNum,
-                        WaterType = "",
+                        WaterType = item.WaterType,
                         BindTag = "",
                         AquariumId = item.Id,
                         temperature = "尚未擁有資料",
